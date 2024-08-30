6fbd39eb22d3a63f1be6727caad32fcb8fff0b45
María José Moreno Herrera <l23590244@sjuanrio.tecnm.mx>
2024-08-29 21:30:25 -0600

    Add files via upload

---
+++ UnidadUno/Objeto.py
@@ -0,0 +1,29 @@
+'''
+Objeto:Biblioteca 
+Clases:Estantes, libros y sillas
+Abstraccion:
+    Estantes:Estante a, estante b, estante c
+    Libros:Titulo x, autor x
+    Sillas:Color cafe, material metal
+Encapsulamiento:
+    Estante:
+        Interfas:Entrada:Guardar libro Salida:Sacar un libro
+        Implementacion:Guardar libros
+    Libros:
+        Interfas:Entrada:Abrir libro Salida:Cerrar libro
+        Implementacion:
+    Sillas:
+        Interfas:Entrada:_____ Salida:_____
+        Implementacion:Guardar libros
+Herencia:
+    Estantes:
+        Herencia:
+        Agregacion:
+    Libros:
+        Herencia:
+        Agregacion:Marcadores, postit y separador
+    Sillas:
+        Herencia:Silla
+        Agregacion:Cambio de color 
+        
+'''
---
+++ UnidadUno/PreguntarNombre.py
@@ -0,0 +1,14 @@
+class name:
+    def __init__(self, user):
+        self.u = user
+class saludo:
+    def __init__(self, name):
+        self.h = name
+ 
+x = name (input('Como te llamas? '))
+s= saludo (f'Hola')
+print (s.h, x.u)
+
+
+
+
